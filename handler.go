74f0d6a66d0f267225e9b01a34b7f15160f20fec
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-08-20 16:48:06 +0000

    fix test

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -17,16 +18,23 @@ func NewHandler(r Repository) *Handler {
 	}
 }
 
+func (h *Handler) get(id string) ([]byte, error) {
+	user, err := h.repo.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(user)
+}
+
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		id := r.URL.Query().Get("id")
-		if user, err := h.repo.Get(id); err == nil {
-			bytes, _ := json.Marshal(user)
+		if bytes, err := h.get(id); err == nil {
 			w.Write(bytes)
 			return
 		}
 	} else if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
 		return
 	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
---
+++ handler_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type MockRepository struct{}
+
+func (m *MockRepository) Get(id string) (*User, error) {
+	return &User{
+		ID:        id,
+		Name:      "testname",
+		CreatedAt: time.Time{},
+	}, nil
+
+}
+
+func (m *MockRepository) Set(user User) error {
+	return nil
+}
+
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "/?id=testid", nil)
+	rec := httptest.NewRecorder()
+
+	handler := NewHandler(&MockRepository{})
+	handler.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("Status code should be 200, but %d\n", rec.Code)
+	}
+
+	got := rec.Body.String()
+	want := `{"id":"testid","name":"testname","created_at":"0001-01-01T00:00:00Z"}`
+	if got != want {
+		t.Errorf("got: %#v, want: %#v", got, want)
+	}
+}
